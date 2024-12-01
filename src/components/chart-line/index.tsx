d06ead939d8d67d4879b2e1b58d8118b9454788d
Thiago Piaia <edirlpiaia@gmail.com>
2024-12-01 17:45:55 -0300

    commit message

---
+++ src/components/chart-line/index.tsx
@@ -0,0 +1,92 @@
+import {
+    CartesianGrid,
+    Line,
+    LineChart,
+    XAxis,
+    YAxis,
+    ResponsiveContainer,
+} from "recharts";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import {
+    ChartConfig,
+    ChartContainer,
+    ChartTooltip,
+    ChartTooltipContent,
+} from "@/components/ui/chart";
+
+const chartData = [
+    { month: "min. 0", desktop: 0, mobile: 0 },
+    { month: "Spot", desktop: 5, mobile: 130 },
+    { month: "min. 10", desktop: 0, mobile: 140 },
+];
+
+const chartConfig = {
+    desktop: {
+        label: "Desktop",
+        color: "hsl(var(--chart-1))",
+    },
+    mobile: {
+        label: "Mobile",
+        color: "hsl(var(--chart-2))",
+    },
+} satisfies ChartConfig;
+
+export function ChartLine() {
+    return (
+        <ResponsiveContainer>
+            <Card className="">
+                <CardHeader> 
+                    <CardDescription>January - June 2024</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <ChartContainer className="h-[100px] w-[700px]" config={chartConfig}>
+                            <LineChart
+                                data={chartData}
+                                margin={{
+                                    top: 10,
+                                    right: 20,
+                                    left: 20,
+                                    bottom: 10,
+                                }}
+                            >
+                                <XAxis
+                                    dataKey="month"
+                                    tickLine={false}
+                                    axisLine={true}
+                                    tickMargin={8}
+                                    tickFormatter={(value) => value.slice(0, 3)}
+                                />
+                                <YAxis />
+                                <ChartTooltip
+                                    cursor={false}
+                                    content={<ChartTooltipContent hideLabel />}
+                                />
+                                <Line
+                                    dataKey="desktop"
+                                    type="natural"
+                                    stroke="var(--color-desktop)"
+                                    strokeWidth={2}
+                                    dot={{
+                                        fill: "var(--color-desktop)",
+                                    }}
+                                    activeDot={{
+                                        r: 6,
+                                    }}
+                                />
+                            </LineChart>
+                    </ChartContainer>
+                </CardContent>
+                <CardFooter className="flex-col items-start gap-2 text-sm">
+                    <p>BaseLine</p>
+                </CardFooter>
+            </Card>
+        </ResponsiveContainer>
+    );
+}
--- src/pages/basic.tsx
+++ src/pages/basic.tsx
@@ -1,8 +1,41 @@
-import DataTableDemo from "../components/table/intex";
+import { ChartLine } from "@/components/chart-line";
+import { Component } from "../components/chart/index";
 import 'tailwindcss/tailwind.css';
 
-export default function Basic(){
-    return(
-        <DataTableDemo/>
+export default function Basic() {
+    return (
+        <div className="grid grid-cols-3 gap-2 gap-y-11 grid-rows-[auto]">
+            <div className="col-span-2 row-span-1">
+                <h2 className="text-4xl">Short term view</h2>
+            </div>
+            <div className="bg-zinc-100/70 rounded-3xl p-3 col-span-2">
+                <div className="rounded-2xl bg-white p-2 px-3 ">
+                    <Component />
+                </div>
+            </div>
+            <div className="bg-zinc-100/70 rounded-3xl p-3 grid grid-cols-2 grid-rows-2 gap-3">
+                <div className="bg-white w-full h-full rounded-3xl flex flex-col justify-center items-center text-center p-3 gap-2">
+                    <h1 className="text-3xl font-bold">0%</h1>
+                    <p className="text-sm">KPI performance in the period</p>
+                </div>
+                <div className="bg-white w-full h-full rounded-3xl flex flex-col justify-center items-center text-center p-3 gap-2">
+                    <h1 className="text-3xl font-bold">7,37k</h1>
+                    <p className="text-sm">KPI total in the period</p>
+                </div>
+                <div className="bg-white w-full h-full rounded-3xl flex flex-col justify-center items-center text-center p-3 gap-2">
+                    <h1 className="text-3xl font-bold">0</h1>
+                    <p className="text-sm">Airtime</p>
+                </div>
+                <div className="bg-white w-full h-full rounded-3xl flex flex-col justify-center items-center text-center p-3 gap-2">
+                    <h1 className="text-3xl font-bold">0</h1>
+                    <p className="text-sm">Syncs</p>
+                </div>
+            </div>
+            <div className="bg-zinc-100/70 rounded-3xl p-3 col-span-2">
+                <div className="flex items-center justify-center h-full max-h-[200px]">
+                    <ChartLine />
+                </div>
+            </div>
+        </div>
     )
 }
