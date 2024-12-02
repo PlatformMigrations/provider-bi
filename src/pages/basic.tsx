65243c5cfe41fa09c6ac178cf10bdd3b6b9cc71c
Flathin <flathinsees@gmail.com>
2024-12-01 22:41:49 -0300

    add style

--- src/pages/basic.tsx
+++ src/pages/basic.tsx
@@ -3,6 +3,12 @@ import 'tailwindcss/tailwind.css';
 
 export default function Basic(){
     return(
-        <DataTableDemo/>
+
+        <div className="bg-white/20 p-4 rounded-3xl">
+            <div className="bg-white p-4 rounded-2xl">
+                <DataTableDemo/>
+            </div>
+        </div>
+        
     )
 }
